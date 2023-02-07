8460f354e2e0039110880da0276e1dcb2f13ff4d
Érik Eiji <eriknibe1@gmail.com>
2023-02-07 15:24:35 -0300

    fix: duration and price type

--- createTables.sql
+++ createTables.sql
@@ -2,6 +2,6 @@ CREATE TABLE IF NOT EXISTS movies_info(
 	id BIGSERIAL PRIMARY KEY,
 	name VARCHAR(50) UNIQUE NOT NULL,
 	description TEXT,
-	duration BIGINT NOT NULL,
-	price BIGINT NOT NULL
+	duration INTEGER NOT NULL,
+	price INTEGER NOT NULL
 );
